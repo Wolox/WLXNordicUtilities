ecfdd58fc37d94a27477653a4e744eb2297be5d8
Guido Marucci Blas <guidomb@gmail.com>
2015-10-08 01:17:03 -0300

    Adds public headers to umbrella header.

--- WLXNordicUtilities/WLXNordicUtilities.h
+++ WLXNordicUtilities/WLXNordicUtilities.h
@@ -17,3 +17,6 @@ FOUNDATION_EXPORT const unsigned char WLXNordicUtilitiesVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WLXNordicUtilities/PublicHeader.h>
 
 
+#import "WLXNRF51XXDeviceManager.h"
+#import "WLXFirmwareUploader.h"
+#import "WLXFirmwareArchive.h"
